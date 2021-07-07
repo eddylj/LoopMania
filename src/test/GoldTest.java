de62586f6a12d5fc7d43abc7a8cc544a5e66dc95
ElliotRotenstein <67740921+ElliotRotenstein@users.noreply.github.com>
2021-07-07 18:10:40 +1000

    Added gold, character and experience unit tests

---
+++ src/test/GoldTest.java
@@ -0,0 +1,86 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import unsw.loopmania.CampfireCard;
+import unsw.loopmania.TrapCard;
+import unsw.loopmania.VampireCastleCard;
+
+public class GoldTest {
+    @Test
+    public void GetGoldFromSlugTest() {
+        List<Enemy> enemies = new ArrayList<Enemy>();
+        Enemy slug = new Slug();
+        Character c = new Character(slug, enemies);
+        assertEquals(0, c.getGold());
+        c.fight();
+        assertEquals(100, c.GetGold());
+    }
+    
+    @Test
+    public void GetGoldFromVampireTest() {
+        List<Enemy> enemies = new ArrayList<Enemy>();
+        Enemy vampire = new Vampire();
+        Character c = new Character(vampire, enemies);
+        assertEquals(0, c.getGold());
+        c.fight();
+        assertEquals(500, c.GetGold());
+    }
+    @Test
+    public void GetGoldFromZombieTest() {
+        List<Enemy> enemies = new ArrayList<Enemy>();
+        Enemy zombie = new Zombie();
+        Character c = new Character(zombie, enemies);
+        assertEquals(0, c.getGold());
+        c.fight();
+        assertEquals(250, c.GetGold());
+    }
+
+    @Test
+    public void getGoldFromSellingTest() {
+        Item sword = new Sword(10);
+        List<Item> equipment = new ArrayList<Item>();
+        equipment.add(sword);
+        Character c = new Character(equipment);
+        assertEquals(0, c.getGold());
+        c.sellItem(sword);
+        assertEquals(sword.getSellPrice(), c.getGold());
+        Item sword2 = new Sword(2);
+        assertTrue(sword.getSellPrice() > sword2.getSellPrice());
+    }
+
+    @Test
+    public void getGoldFromItemReplaceTest() {
+        List<Item> equipment = new ArrayList<Item>();
+        Item shield = new Shield(3);
+        equipment.add(shield);
+        for (int i = 0; i < 14; i++) {
+            equipment.add(new Helmet(5));
+        }
+        Character c = new Character(equipment);
+        Item sword = new Sword(1);
+        assertEquals(0, c.getGold());
+        c.pickUp(sword);
+        assertEquals(shield.getReplaceCost(), c.getGold());
+    }
+
+    @Test
+    public void getGoldFromCardReplaceTest() {
+        Character c = new Character();
+        for (int i = 0; i < 10; i++) {
+            c.pickup(new TrapCard());
+        }
+        assertEquals(0, c.getGold());
+        c.pickup(new CampfireCard());
+        assertTrue(300 <= c.getGold() && c.getGold() <= 400);
+        c.pickup(new VampireCastleCard())
+        assertTrue(600 <= c.getGold() && c.getGold() <= 800);
+    }
+}
