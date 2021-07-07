b9bb543389bf01b6a1801cb7db5178c46db89c16
Kevin <z5311917@ad.unsw.edu.au>
2021-07-07 23:34:32 +1000

    added more classes from UML

---
+++ src/unsw/battleMediator.java
@@ -0,0 +1,26 @@
+
+
+
+    /**
+     * run the expected battles in the world, based on current world state
+     * @return list of enemies which have been killed
+     */
+    public List<BasicEnemy> runBattles() {
+        // TODO = modify this - currently the character automatically wins all battles without any damage!
+        List<BasicEnemy> defeatedEnemies = new ArrayList<BasicEnemy>();
+        for (BasicEnemy e: enemies){
+            // Pythagoras: a^2+b^2 < radius^2 to see if within radius
+            // TODO = you should implement different RHS on this inequality, based on influence radii and battle radii
+            if (Math.pow((character.getX()-e.getX()), 2) +  Math.pow((character.getY()-e.getY()), 2) < 4){
+                // fight...
+                defeatedEnemies.add(e);
+            }
+        }
+        for (BasicEnemy e: defeatedEnemies){
+            // IMPORTANT = we kill enemies here, because killEnemy removes the enemy from the enemies list
+            // if we killEnemy in prior loop, we get java.util.ConcurrentModificationException
+            // due to mutating list we're iterating over
+            killEnemy(e);
+        }
+        return defeatedEnemies;
+    }
