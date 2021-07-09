a0b1b27068b6c85ab9a28bd460eaac711fd10cae
Kevin <z5311917@ad.unsw.edu.au>
2021-07-09 12:00:34 +1000

    added seperate files for building tests

---
+++ src/test/VampireCastleTest.java
@@ -0,0 +1,16 @@
+package test;
+import unsw.loopmania.Character;
+import unsw.loopmania.Vampire;
+import unsw.loopmania.Enemy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+public class VampireCastleTest {
+    
+}
