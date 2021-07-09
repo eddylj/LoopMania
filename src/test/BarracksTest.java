35f9718bac51c4d635bdc8b17079f4e376cca9b1
Kevin <z5311917@ad.unsw.edu.au>
2021-07-09 13:55:32 +1000

    added barrackstests

--- src/test/BarracksTest.java
+++ src/test/BarracksTest.java
@@ -1,4 +1,5 @@
 package test;
+import unsw.loopmania.BarracksBuilding;
 import unsw.loopmania.Character;
 import unsw.loopmania.Vampire;
 import unsw.loopmania.Enemy;
@@ -12,5 +13,25 @@ import java.util.List;
 
 import org.junit.Test;
 public class BarracksTest {
-    
+    @Test
+    public void BarracksTest() {
+        BarracksBuilding Barracks = new BarracksBuilding();
+        Character c = new Character();
+        Barracks.addAlliedSoldier();
+        List aList  = c.getAlliedSoldiers();
+        assertEquals(aList.size(), 1);
+    }
+    @Test
+    public void MaxAlliedSoldierTest() {
+        BarracksBuilding Barracks = new BarracksBuilding();
+        Character c = new Character();
+        Barracks.addAlliedSoldier();
+        Barracks.addAlliedSoldier();
+        Barracks.addAlliedSoldier();
+        Barracks.addAlliedSoldier();
+        Barracks.addAlliedSoldier();
+        Barracks.addAlliedSoldier();
+        List aList  = c.getAlliedSoldiers();
+        assertEquals(aList.size(), 5);
+    }
 }
