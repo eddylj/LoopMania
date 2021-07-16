912e3b82964aa9e1f6d4148f88c27ad357001b7b
Kevin <z5311917@ad.unsw.edu.au>
2021-07-16 11:48:18 +1000

    Added Tests for allied soldier

--- src/test/AlliedSoldiersTest.java
+++ src/test/AlliedSoldiersTest.java
@@ -3,19 +3,32 @@ package test;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import unsw.loopmania.Character;
-import unsw.loopmania.Enemy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import unsw.loopmania.Slug;
 import unsw.loopmania.AlliedSoldier;
 
 public class AlliedSoldiersTest {
+    AlliedSoldier a = new AlliedSoldier(1);
 
-    
+    @Test
+    public void isDeadTest(){
+        assertEquals(a.getHealth(), 30);
+        a.setHealth(0);
+        assertTrue(a.isDead());
+    }
 
+    @Test
+    public void attackTest(){
+        Slug s = new Slug();
+        a.attack(s);
+        assertEquals(s.getHealth(), 29);
+    }
 
-
-
-
+    @Test
+    public void takeDamageTest() {
+        Slug s = new Slug();
+        s.attack(a);
+        assertEquals(a.getHealth(), 20);
+    }
 }
 
