bdde98c206f9cba6329637805d0e4c23dcf82513
ElliotRotenstein <67740921+ElliotRotenstein@users.noreply.github.com>
2021-07-18 20:27:41 +1000

    Added even more javadoc. Committing to get Kevin's javadoc

--- src/unsw/loopmania/Character.java
+++ src/unsw/loopmania/Character.java
@@ -128,14 +128,22 @@ public class Character extends MovingEntity implements Hero {
 
     // STORE STUFF
     //////////////////////////////////
+
+    /**
+     * Sells an item and gives the character some gold
+     * @param i Item: item to be sold
+     */
     public void sellItem(Item i) {
-        
     }
     ////////////////////////////////////////
 
     // INVENTORY ITEM RELATED STUFF
     ////////////////////////////////////
 
+    /**
+     * Checks whether player has The One Ring in their inventory
+     * @return Boolean depending on whether player has the Ring
+     */
     public boolean hasRing() {
         return inventory.hasRing();
     }
@@ -146,15 +154,24 @@ public class Character extends MovingEntity implements Hero {
 
     // SOLDIER STUFF
     ////////////////////////////////////
+    /**
+     * Removes any dead allied soldiers from the soldier list.
+     * This is called after the battle has been done
+     */
     public void updateAlliedSoldierAmount() {
         for (AlliedSoldier s : soldiers) {
             if (s.getHealth() <= 0) {
                 soldiers.remove(s);
             }
         }
-        aliveSoldiers.set(soldiers.size()); // observer pattern wooo
+        aliveSoldiers.set(soldiers.size());
     }
 
+    /**
+     * Adds an allied soldier to the character's ally list
+     * if there is space
+     * @param soldier AlliedSoldier: soldier to be added to the list
+     */
     public void addAlliedSoldier(AlliedSoldier soldier) {
         if (aliveSoldiers.getValue() < 3) {
             soldiers.add(0,soldier);
@@ -165,84 +182,198 @@ public class Character extends MovingEntity implements Hero {
 
     // Getters and Setters and other incrementors
     ///////////////////////////////////////////////////////
+    /**
+     * Gets the character's health
+     * @return int: The character's health
+     */
     public int getHealth() {
         return health;
     }
+
+    /**
+     * Gets the character's experience
+     * @return int: The character's experience
+     */
     public int getXP() {
         return experience;
     }
+
+    /**
+     * Gets the character's gold
+     * @return int: The character's gold
+     */
     public int getGold() {
         return gold;
     }
+
+    /**
+     * Gets the character's cycles
+     * @return int: The character's cycles
+     */
     public int getCycles() {
         return cycles;
     }
+
+    /**
+     * Gets the character's allied soldier count
+     * @return int: The character's allied soldier count
+     */
     public int getAlliedSoldierCount() {
         return aliveSoldiers.get();
     }
-    public List<AlliedSoldier> getSoldiers() {
-        return soldiers;
-    }
+
+    /**
+     * Gets the character's allied soldiers
+     * @return List<AlliedSoldier>: The character's allied soldiers
+     */
     public List<AlliedSoldier> getAlliedSoldiers() {
         return soldiers;
     }
+
+    /**
+     * Gets the character's bonus damage strategy
+     * @return BonusDamageStrategy: The character's bonus damage strategy
+     */
     public BonusDamageStrategy getBonusDamageStrategy() {
         return appliedBuff;
     }
+
+    /**
+     * Gets the character's item level stats
+     * @return CharacterStats: The character's item level stats class
+     */
     public CharacterStats getStats() {
         return stats;
     }
+
+    /**
+     * Sets the character's bonus damage strategy
+     * @param buff BonusDamageStrategy: The selected BonusDamageStrategy
+     */
     public void setBonusDamageStrategy(BonusDamageStrategy buff) {
         appliedBuff = buff;
     }
-    public void setHealth(int i) {
-        health = i;
+
+    /**
+     * Sets the character's health (used so health doesn't go above 100)
+     * @param health int: The character's health
+     */
+    public void setHealth(int health) {
+        this.health = health;
     } 
+
+    /**
+     * Increases the character's health
+     * @param health int: The character's maximum health increase
+     */
     public void restoreHealth(int amount) {
         health += amount;
         if (health > 100) {
             setHealth(100);
         }
     }
+
+    /**
+     * Increases the character's gold amount
+     * @param amount int: Amount to increase
+     */
     public void gainGold(int amount) {
         gold += amount;
     }
+
+    /**
+     * Decreases the character's gold amount
+     * @param amount int: Amount to decrease
+     */
     public void loseGold(int amount) {
         gold -= amount;
     }
+
+    /**
+     * Increases the character's experience amount
+     * @param amount int: Amount to increase
+     */
     public void gainXP(int amount) {
         experience += amount;
     }
+    /**
+     * Increases character's cycle count by 1
+     */
     public void gainCycle() {
         cycles++;
     }
+
+    /**
+     * Decreases the character's health
+     * @param damage double: Amount to decrease
+     */
     public void loseHealth(double damage){
         health -= (int)damage;
     }
+
+    /**
+     * Gets highest level of a particular type of item
+     * @param item Item: Item who's highest level is being inquired
+     * @return int: Highest level for item
+     */
     public int getHighestLevel(Item item) {
         return stats.getHighestLevel(((StaticEntity)item).getType());
     }
+
+    /**
+     * Gets highest level of a particular type of item
+     * @param item Item: Item who's highest level is being inquired
+     * @return int: Highest level for item
+     */
     public int getHighestLevel(String item) {
         return stats.getHighestLevel(item);
     }
+
+    /**
+     * Updates character's highest level for particular item by 1
+     * @param item Item: Type of item to increase highest level of
+     */
     public void updateHighest(Item item) {
         stats.updateHighestLevel(item);
     }
+
+    /**
+     * Gets currently equipped weapon
+     * @return Item: the currently equipped weapon
+     */
     public Item getWeapon() {
         return equippedWeapon;
     }
+
+    /**
+     * Gets currently equipped shield
+     * @return Item: the currently equipped shield
+     */
     public Item getShield() {
         return equippedShield;
     }
 
+    /**
+     * Gets currently equipped armour
+     * @return Item: the currently equipped armour
+     */
     public Item getArmour() {
         return equippedArmour;
     }
 
+    /**
+     * Gets currently equipped helmet
+     * @return Item: the currently equipped helmet
+     */
     public Item getHelmet() {
         return equippedHelmet;
     }
 
+    /**
+     * Equips a given item. Assumes item is valid to be equipped
+     * (e.g. Item is not a HealthPotion)
+     * @param i Item: item to be equipped.
+     */
     public void equip(Item i) {
         inventory.removeUnequippedItem(i);
         stats.updateHighestLevel(i);
@@ -257,42 +388,110 @@ public class Character extends MovingEntity implements Hero {
         }
     }
 
+    /**
+     * Gets a List of items that don't have levels
+     * @return List<String>: A List of items that don't have levels
+     */
     public List<String> getNonLevelItems() {
         return inventory.getNonLevelItems();
     }
     //////////////////////////////////////////////////////
+
+    /**
+     * Gets a list of all unequipped items in the character's inventory
+     * @return List<Item>: a list of all unequipped items in the character's inventory
+     */
     public List<Item> getunequippedInventoryItems() {
         return inventory.getunequippedInventoryItems();
     }
-    public StaticEntity addUnequippedItem(String string, int i) {
-        return inventory.addUnequippedItem(string, i);
+
+    /**
+     * Adds an item to the player's unequipped inventory
+     * @param type String: Type of item to add
+     * @param i int: Level of item to add
+     * @return StaticEntity: Item added to unequipped inventory
+     */
+    public StaticEntity addUnequippedItem(String type, int level) {
+        return inventory.addUnequippedItem(type, level);
     }
-    public  Inventory getInventory() {
+
+    /**
+     * Gets character's inventory object (used in the shop)
+     * @return Inventory: character's inventory object
+     */
+    public Inventory getInventory() {
         return inventory;
     }
+
+    /**
+     * Gets an item from the character's unequipped inventory from its coordinates
+     * @param x int: Column coordinate in unequipped inventory
+     * @param y int: Row coordinate in unequipped inventory
+     * @return Item: The item
+     */
     public Item getUnequippedInventoryItemEntityByCoordinates(int x, int y){
         return inventory.getUnequippedInventoryItemEntityByCoordinates(x, y);
     }
+
+    /**
+     * Removes an item from the character's unequipped inventory from its coordinates
+     * @param x int: Column coordinate in unequipped inventory
+     * @param y int: Row coordinate in unequipped inventory
+     */
     public void removeUnequippedInventoryItemByCoordinates(int nodeX, int nodeY) {
         inventory.removeUnequippedInventoryItemByCoordinates(nodeX, nodeY);
     }
+
+    /**
+     * Adds a card to the character's card inventory
+     * @param type String: type of card to add
+     * @param width int: Total length of rows
+     * @return StaticEntity: The card that was added
+     */
     public StaticEntity loadCard(String type, int width) {
         return inventory.loadCard(type, width);
     }
+
+    /**
+     * Gets a card from the character's card inventory from its coordinates
+     * @param cardNodeX int: Column coordinate in unequipped inventory
+     * @param cardNodeY int: Row coordinate in unequipped inventory
+     * @return Card: The card
+     */
     public Card getMatchingCard(int cardNodeX, int cardNodeY) {
         return inventory.getMatchingCard(cardNodeX, cardNodeY);
     }
+
+    /**
+     * Removes a card from the character's card inventory
+     * @param card Card: Card to remove
+     * @param cardNodeX int: index of card
+     */
     public void destroyCard(Card card, int cardNodeX) {
         inventory.destroyCard(card, cardNodeX);
     }
+
+    /**
+     * Gets a card from the character's card inventory from its index
+     * @param x int: index of the card
+     * @return Card: The card
+     */
     public Card getCardByCoordinate(int x) {
         return inventory.getCardByCoordinate(x);
     }
 
+    /**
+     * Gets number of unequipped items
+     * @return int: number of unequipped items
+     */
     public int getUnequippedInventoryItemsNum() {
         return inventory.getUnequippedInventoryItemsNum();
     }
 
+    /**
+     * Gets number of cards
+     * @return int: number of cards
+     */
     public int getCardsNum() {
         return inventory.getCardsNum();
     }
