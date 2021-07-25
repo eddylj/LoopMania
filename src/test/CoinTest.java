cd7cac1a6a9effa19ce0e45583c38617a9d679e8
Hugo Sullivan <hugo.sullivan@student.unsw.edu.au>
2021-07-25 21:50:54 +1000

    Fixed CoinTest

--- src/test/CoinTest.java
+++ src/test/CoinTest.java
@@ -3,28 +3,32 @@ package test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Test;
 
 import unsw.loopmania.Coin;
+import unsw.loopmania.LoopManiaWorld;
 import unsw.loopmania.Character;
 
 public class CoinTest {
 
     @Test
     public void checkCoinAmount() {
-        Coin c = new Coin();
-        assertTrue(c.getAmount() >= 50 && c.getAmount() <= 100);
+        for (int i = 0; i < 100; i++) {
+            LoopManiaWorld.setSeed(i);
+            Coin c = new Coin();
+            assertTrue(c.getAmount() >= 50 && c.getAmount() <= 100);
+        }
     }
 
     @Test
     public void checkCharacterGainGold() {
+        LoopManiaWorld.setSeed(1);
         Coin c = new Coin();
         Character character = new Character();
         assertTrue(c.shouldExist().get());
         assertEquals(0, character.getGold());
+        c.activate(character);
         assertEquals(c.getAmount(), character.getGold());
         assertTrue(!c.shouldExist().get());
     }
