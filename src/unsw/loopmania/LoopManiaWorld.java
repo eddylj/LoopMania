ae20080eac52558b57a94d1f9f4096de4110307e
ElliotRotenstein <67740921+ElliotRotenstein@users.noreply.github.com>
2021-07-30 23:46:45 +1000

    Added bosses to goal-conditions

--- src/unsw/loopmania/LoopManiaWorld.java
+++ src/unsw/loopmania/LoopManiaWorld.java
@@ -743,6 +743,10 @@ public class LoopManiaWorld {
         equipItem(iF.create(new SimpleIntegerProperty(0), new SimpleIntegerProperty(0), "sword", 1), "weapon");
         shop = new Shop(character);
         character.setRareItems(rareItems);
+        character.addUnequippedItem("shield", 2);
+        character.addUnequippedItem("shield", 2);
+        character.addUnequippedItem("shield", 2);
+        character.addUnequippedItem("shield", 2);
     }
 
     public void setMode(String mode) {
